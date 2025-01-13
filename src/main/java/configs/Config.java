a7f8f47f8b196f6038435d7579d0ba9524da8e55
Cahigan-MarkLouie <markcahigan54@gmail.com>
2025-01-13 20:35:54 +0800

    add branch

--- src/main/java/configs/Config.java
+++ src/main/java/configs/Config.java
@@ -3,8 +3,8 @@ package configs;
 public class Config {
 	public static final String SCHEMA = "library_tracker2";
 	public static final String USER = "root";
-	public static final String PASSWORD = "Password123!";
+	public static final String PASSWORD = "1234";
 	
-	public static final String LOG_PATH = "/home/von/eclipse-workspace/library_tracker/logs/";
-	public static final String UPLOAD_PATH = "/home/von/eclipse-workspace/library_tracker/src/main/webapp/uploads/";
+	public static final String LOG_PATH = "C:\\xampp\\htdocs\\lib_app_2\\library_tracker_vanilla\\logs";
+	public static final String UPLOAD_PATH = "C:\\xampp\\htdocs\\lib_app_2\\library_tracker_vanilla\\src\\main\\webapp\\uploads";
 }
