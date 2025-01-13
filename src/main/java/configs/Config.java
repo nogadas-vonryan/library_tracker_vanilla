eb613d15cdc68a9571d34bbd57add576fb94d7d7
Cahigan-MarkLouie <markcahigan54@gmail.com>
2025-01-13 23:47:01 +0800

    testing bugs

--- src/main/java/configs/Config.java
+++ src/main/java/configs/Config.java
@@ -5,6 +5,6 @@ public class Config {
 	public static final String USER = "root";
 	public static final String PASSWORD = "1234";
 	
-	public static final String LOG_PATH = "C:\\xampp\\htdocs\\lib_app_2\\library_tracker_vanilla\\logs";
-	public static final String UPLOAD_PATH = "C:\\xampp\\htdocs\\lib_app_2\\library_tracker_vanilla\\src\\main\\webapp\\uploads";
+	public static final String LOG_PATH = "C:\\xampp\\htdocs\\lib_app_2\\library_tracker_vanilla\\logs\\";
+	public static final String UPLOAD_PATH = "C:\\xampp\\htdocs\\lib_app_2\\library_tracker_vanilla\\src\\main\\webapp\\uploads\\";
 }
